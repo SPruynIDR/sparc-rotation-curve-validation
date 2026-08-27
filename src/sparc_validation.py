bdeeb898670c8a6f1342aafde3241e5c5a4b41fd
SPruynIDR <shawnpruyn77@gmail.com>
2026-08-26 21:23:21 -0400

    Fix typo in TABLE2_CANDIDATES definition

--- src/sparc_validation.py
+++ src/sparc_validation.py
@@ -45,7 +45,7 @@ TABLE1_CANDIDATES = {
     "Q": ["Q"],
 }
 TABLE2_CANDIDATES = {
-    "name": ["Galaxy", "Name"],
+    "name": ["Galaxy", "Name" "ID"],
     "r": ["R", "r", "Rad"],
     "Vobs": ["Vobs", "V_obs"],
     "e_Vobs": ["e_Vobs"],
